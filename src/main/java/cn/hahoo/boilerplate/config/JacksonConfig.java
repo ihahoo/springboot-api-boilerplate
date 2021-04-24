05978e39727eac8b2f630bd337762cda6d87b0fa
Ray Guo <ihahoo@163.com>
2021-04-24 14:16:19 +0800

    修改Jackson输出对象为空值时显示为空字串

--- src/main/java/cn/hahoo/boilerplate/config/JacksonConfig.java
+++ src/main/java/cn/hahoo/boilerplate/config/JacksonConfig.java
@@ -1,16 +1,21 @@
 package cn.hahoo.boilerplate.config;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.io.IOException;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
+    @Primary
     public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
         return new Jackson2ObjectMapperBuilder()
                 .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
@@ -18,5 +23,19 @@ public class JacksonConfig {
                 .simpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
                 .timeZone("UTC")
                 .propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+    }
+
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+            @Override
+            public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+                jsonGenerator.writeString("");
+            }
+        });
+        return objectMapper;
     }
 }
